84bf131a53dd04fc295a03ae7ac34a69fec6d557
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-07-17 16:44:25 +0200

    Changed the whole structure to the repository.

--- Chapter 1 - Computer Programming/src/Goodbye_03.java
+++ Chapter 1 - Computer Programming/src/Goodbye_03.java
@@ -1,5 +1,5 @@
 
-public class Goodbye {
+public class Goodbye_03 {
 
 	public static void main(String[] args) {
 		System.out.print("Goodbye, ");
--- Chapter 1 - Computer Programming/src/HowAreYou_02.java
+++ Chapter 1 - Computer Programming/src/HowAreYou_02.java
@@ -1,5 +1,5 @@
 
-public class HowAreYou {
+public class HowAreYou_02 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
