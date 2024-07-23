727843652f179de67084a272e84dea1ef4fceb76
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-07-23 10:40:51 +0200

    Added a new class.

---
+++ Chapter 2 - Variables and Operators/src/TimeExercise.java
@@ -0,0 +1,18 @@
+public class TimeExercise {
+
+    public static void main(String[] args) {
+        int hour = 10;
+        int minute = 25;
+        int second = 47;
+
+        int midnightSeconds = (hour * 3600) + (minute * 60) + second;
+        System.out.println("Time since midnight: " + midnightSeconds + " seconds");
+
+        int dailySeconds = 86400;
+        int secondsRemaining = dailySeconds - midnightSeconds;
+        System.out.println("Seconds remaining in the day: " + secondsRemaining + " seconds");
+
+        double dayPercentage = ((double) midnightSeconds / dailySeconds) * 100;
+        System.out.println("Percentage of day that has passed: " + String.format("%.2f", dayPercentage) + "%");
+    }
+}
