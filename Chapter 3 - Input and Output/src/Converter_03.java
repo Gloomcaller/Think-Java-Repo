dedf7a66ed87a520481db2b92b8033ba183e8db2
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-07-29 22:57:16 +0200

    Added the Converter example.

---
+++ Chapter 3 - Input and Output/src/Converter_03.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Converter_03 {
+
+	public static void main(String[] args) {
+
+		final double CM_PER_INCH = 2.54;
+		final int IN_PER_FOOT = 12;
+
+		Scanner in = new Scanner(System.in);
+
+		System.out.print("Exactly how many cm? ");
+		double cm = in.nextDouble();
+
+		int totalInches = (int) (cm / CM_PER_INCH);
+
+		int feet = totalInches / IN_PER_FOOT;
+		int inches = totalInches % IN_PER_FOOT;
+
+		System.out.printf("%.2f cm = %d ft, %d in\n", cm, feet, inches);
+
+		double meters = cm / 100;
+		double kilometers = meters / 1000;
+
+		System.out.printf("%.2f cm = %.2f meters\n", cm, meters);
+		System.out.printf("%.2f cm = %.5f kilometers\n", cm, kilometers);
+
+		System.out.print("Enter inches to convert to cm: ");
+		double inputInches = in.nextDouble();
+		double convertedCm = inputInches * CM_PER_INCH;
+		System.out.printf("%.2f inches = %.2f cm\n", inputInches, convertedCm);
+
+		in.close();
+	}
+}
