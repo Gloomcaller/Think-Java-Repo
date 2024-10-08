938f8dc18f09bf5f069a15530cd571ffa93fc0de
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-10-08 20:51:26 +0200

    New class.

---
+++ Chapter 4 - Methods and Testing/src/DefiningMethods_01.java
@@ -0,0 +1,13 @@
+public class DefiningMethods_01 {
+
+	public static void newLine() {
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("First line.");
+		newLine();
+		System.out.println("Second Line.");
+	}
+
+}
