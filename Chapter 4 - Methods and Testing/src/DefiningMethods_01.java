a734572c5b1d7cfb23a568f489a6b6f07df2701d
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-10-08 21:37:22 +0200

    New lines

--- Chapter 4 - Methods and Testing/src/DefiningMethods_01.java
+++ Chapter 4 - Methods and Testing/src/DefiningMethods_01.java
@@ -4,10 +4,18 @@ public class DefiningMethods_01 {
 		System.out.println();
 	}
 
+	public static void treeLine() {
+		newLine();
+		newLine();
+		newLine();
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("First line.");
 		newLine();
 		System.out.println("Second Line.");
+		treeLine();
+		System.out.println("Third Line.");
 	}
 
 }
