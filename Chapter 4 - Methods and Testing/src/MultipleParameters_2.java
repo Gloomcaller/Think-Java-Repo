7dc90bda0dd3a0dfab5a05e9959c8eb5781e9734
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-10-09 22:14:23 +0200

    Next Class

---
+++ Chapter 4 - Methods and Testing/src/MultipleParameters_2.java
@@ -0,0 +1,18 @@
+
+public class MultipleParameters_2 {
+
+	public static void printTime(int hour, int minute) {
+		System.out.print(hour);
+		System.out.print(":");
+		System.out.println(minute);
+	}
+
+	public static void main(String[] args) {
+		
+		int hour = 11;
+		int minute = 59;
+		printTime(hour, minute);
+		
+	}
+
+}
