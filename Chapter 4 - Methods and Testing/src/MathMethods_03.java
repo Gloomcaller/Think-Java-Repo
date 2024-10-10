d90a63d724b16e707b791df4da4246f86abbe3da
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-10-10 14:13:52 +0200

    Little EX

--- Chapter 4 - Methods and Testing/src/MathMethods_03.java
+++ Chapter 4 - Methods and Testing/src/MathMethods_03.java
@@ -8,12 +8,18 @@ public class MathMethods_03 {
 		double height = Math.sin(angle);
 
 		double degrees = 90;
-		double angle2 = degrees / 180.0 * Math.PI;
+		double angleToRadiants = degrees / 180.0 * Math.PI;
 
 		double radians = Math.toRadians(180.0);
-		double degrees2 = Math.toDegrees(Math.PI);
+		double degreesToDeg = Math.toDegrees(Math.PI);
 
-		long x = Math.round(Math.PI * 20.0);
+		long xRounding = Math.round(Math.PI * 20.0);
+
+		double xCos = Math.cos(angle + Math.PI / 2.0);
+
+		double xExponential = Math.exp(Math.log(10.0));
+		
+		double xPower = Math.pow(2.0, 10.0);
 	}
 
 }
