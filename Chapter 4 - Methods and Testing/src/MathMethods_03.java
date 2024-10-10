5e3393c1307bc33ee0c2b1e70c93eb2d063d1d1a
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-10-10 14:06:01 +0200

    New class

--- Chapter 4 - Methods and Testing/src/MathMethods_03.java
+++ Chapter 4 - Methods and Testing/src/MathMethods_03.java
@@ -2,8 +2,18 @@
 public class MathMethods_03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		double root = Math.sqrt(17.0);
+		double angle = 1.5;
+		double height = Math.sin(angle);
+
+		double degrees = 90;
+		double angle2 = degrees / 180.0 * Math.PI;
+
+		double radians = Math.toRadians(180.0);
+		double degrees2 = Math.toDegrees(Math.PI);
+
+		long x = Math.round(Math.PI * 20.0);
 	}
 
 }
