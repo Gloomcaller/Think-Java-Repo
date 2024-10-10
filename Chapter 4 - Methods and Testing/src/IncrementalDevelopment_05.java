e10fb96a2bc37e59e57fc2262dd98ba0e5d3828f
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-10-10 14:51:00 +0200

    Done with this one

--- Chapter 4 - Methods and Testing/src/IncrementalDevelopment_05.java
+++ Chapter 4 - Methods and Testing/src/IncrementalDevelopment_05.java
@@ -4,15 +4,16 @@ public class IncrementalDevelopment_05 {
 	public static double distance(double x1, double y1, double x2, double y2) {
 		double dx = x2 - x1;
 		double dy = y2 - y1;
-		System.out.print("dx is " + dx);
-		System.out.print("dy is " + dy);
-		return 0.0;
+		double dsquared = dx * dx + dy + dy;
+		double result = Math.sqrt(dsquared);
+		return result;
 	}
 
 	public static void main(String[] args) {
 
 		double dist = distance(1.0, 2.0, 4.0, 6.0);
 		System.out.print(dist);
+
 	}
 
 }
