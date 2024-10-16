e69c1c0d3cd30f4c5974e29347178a7f31c14fda
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-10-16 14:44:06 +0200

    Logical Operators and De Morgan's Laws.

---
+++ Chapter 5 - Conditionals and Logic/src/LogicalLaws_03.java
@@ -0,0 +1,32 @@
+
+public class LogicalLaws_03 {
+
+	public static void main(String[] args) {
+		int x = 5;
+		boolean A = x > 0;
+		boolean B = x < 10;
+
+		// Logical operators
+		if (x > 0 && x < 10) {
+			System.out.println("x is greater than 0 and less than 10");
+		}
+
+		if (x < 0 || x > 10) {
+			System.out.println("x is either less than 0 or greater than 10");
+		}
+
+		if (!(x > 0)) {
+			System.out.println("x is not greater than 0");
+		}
+
+		// De Morgan's Laws
+		if (!(A && B)) {
+			System.out.println("!(A && B) is true, which is the same as !A || !B");
+		}
+
+		if (!(A || B)) {
+			System.out.println("!(A || B) is true, which is the same as !A && !B");
+		}
+	}
+
+}
