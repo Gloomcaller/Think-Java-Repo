221b9be030e9e9c14b93b4fbd4eef9971a763a33
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-10-21 15:58:36 +0200

    Abecedarian check.

--- Chapter 6 - Loops and Strings/src/chapter6exercises/Exercise64.java
+++ Chapter 6 - Loops and Strings/src/chapter6exercises/Exercise64.java
@@ -2,22 +2,20 @@ package chapter6exercises;
 
 public class Exercise64 {
 	public static boolean isAbecedarian(String str) {
-		for (int i = 0; i < str.length(); i++) {
-			if (str.indexOf(i) < str.indexOf(i + 1)) {
-			} else {
+		for (int i = 0; i < str.length() - 1; i++) {
+			if (str.charAt(i) > str.charAt(i + 1)) {
 				return false;
 			}
 		}
-		return false;
+		return true;
 	}
 
 	public static void main(String[] args) {
-		String word = "word";
-		boolean abecedrian = isAbecedarian(word);
+		String word = "exercise";
+		boolean abecedrian = isAbecedarian(word.toLowerCase());
 		if (abecedrian)
-			System.out.println("The word " + word + " is Abecedrian.");
+			System.out.println("The word " + word + " is Abecedarian.");
 		else
-			System.out.println("The word " + word + " is Abecedrian.");
+			System.out.println("The word " + word + " is not Abecedarian.");
 	}
-
 }
