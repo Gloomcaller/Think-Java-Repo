7cae5e94e15b9d8f4adf95b74e00ea5b8442c926
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-10-23 09:24:23 +0200

    Anagrams.

--- Chapter 7 - Arrays and References/src/chapter7exercises/Exercise78.java
+++ Chapter 7 - Arrays and References/src/chapter7exercises/Exercise78.java
@@ -1,5 +1,7 @@
 package chapter7exercises;
 
+import java.util.Arrays;
+
 public class Exercise78 {
 
 	public static int[] letterHist(String str) {
@@ -13,13 +15,19 @@ public class Exercise78 {
 		}
 		return histogram;
 	}
-	
+
 	public static void main(String[] args) {
 		String str1 = "christopher mayfield";
 		String str2 = "hi prof the camel is dry";
-		
-		int[] his1 = letterHist
-		
+
+		int[] his1 = letterHist(str1);
+		int[] his2 = letterHist(str2);
+
+		if (Arrays.equals(his1, his2))
+			System.out.println("Our two strings are anagrams.");
+		else
+			System.out.println("Our two strings are not anagrams.");
+
 	}
 
 }
