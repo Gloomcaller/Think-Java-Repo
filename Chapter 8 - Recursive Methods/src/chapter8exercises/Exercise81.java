7ca3ec658c0a4ed8a6d67f93bd10dfdeada2eab6
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-10-30 08:44:33 +0100

    Code cleanup.

--- Chapter 8 - Recursive Methods/src/chapter8exercises/Exercise81.java
+++ Chapter 8 - Recursive Methods/src/chapter8exercises/Exercise81.java
@@ -9,19 +9,20 @@ public class Exercise81 {
 			System.out.println("ya’ can’t take one down, ya’ can’t pass it around,");
 			System.out.println("’cause there are no more bottles of beer on the wall!");
 		} else {
-			System.out.println(bottles + " bottles of beer on the wall,");
-			System.out.println(bottles + " bottles of beer,");
+			String bottleWord = (bottles == 1) ? "bottle" : "bottles";
+			String nextBottleWord = (bottles - 1 == 1) ? "bottle" : "bottles";
+
+			System.out.println(bottles + " " + bottleWord + " of beer on the wall,");
+			System.out.println(bottles + " " + bottleWord + " of beer,");
 			System.out.println("ya’ take one down, ya’ pass it around,");
-			System.out.println((bottles - 1) + " bottles of beer on the wall.");
+			System.out.println((bottles - 1) + " " + nextBottleWord + " of beer on the wall.");
+
 			beerSong(bottles - 1);
 		}
-
 	}
 
 	public static void main(String[] args) {
-		int bottles = 5;
+		int bottles = 99;
 		beerSong(bottles);
-
 	}
-
 }
