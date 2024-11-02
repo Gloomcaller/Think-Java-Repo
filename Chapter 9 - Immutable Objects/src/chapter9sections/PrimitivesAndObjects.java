369893bfdd3c0bf004e13dcead1e1efccbf7b7e4
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2024-11-02 15:14:28 +0100

    Wrappers and Validation

--- Chapter 9 - Immutable Objects/src/chapter9sections/PrimitivesAndObjects.java
+++ Chapter 9 - Immutable Objects/src/chapter9sections/PrimitivesAndObjects.java
@@ -7,16 +7,16 @@ public class PrimitivesAndObjects {
 		int primitiveInt = 5;
 		Integer objectInt = Integer.valueOf(5);
 
-		System.out.println("Primitive int: " + primitiveInt);
-		System.out.println("Integer object: " + objectInt);
+		System.out.println("Primitive int : " + primitiveInt);
+		System.out.println("Integer object : " + objectInt);
 
 		Integer nullableInt = null;
-		System.out.println("Nullable Integer: " + nullableInt);
+		System.out.println("Nullable Integer : " + nullableInt);
 
 		String str1 = "Hello";
 		String str2 = str1;
 		str1 = str1 + " World";
-		System.out.println("Original string changed to: " + str1);
-		System.out.println("str2 remains: " + str2);
+		System.out.println("Original string changed to : " + str1);
+		System.out.println("str2 remains : " + str2);
 	}
 }
---
+++ Chapter 9 - Immutable Objects/src/chapter9sections/WrapperValidation.java
@@ -0,0 +1,37 @@
+package chapter9sections;
+
+import java.math.BigInteger;
+
+public class WrapperValidation {
+
+	public static void main(String[] args) {
+
+		Integer intWrapper = Integer.valueOf(10);
+		Double doubleWrapper = Double.valueOf(20.5);
+		System.out.println("Integer Wrapper : " + intWrapper);
+		System.out.println("Double Wrapper : " + doubleWrapper);
+
+		if (args.length > 0) {
+			System.out.println("First cmd-line argument : " + args[0]);
+		} else {
+			System.out.println("No cmd-line arguments provided.");
+		}
+
+		if (args.length < 2) {
+			System.out.println("Please provide two numbers as arguments.");
+		} else {
+			try {
+				int num1 = Integer.parseInt(args[0]);
+				int num2 = Integer.parseInt(args[1]);
+				System.out.println("Arguments are valid numbers : " + num1 + " and " + num2);
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid number format in arguments.");
+			}
+		}
+
+		BigInteger bigNum1 = new BigInteger("123456789123456789123456789");
+		BigInteger bigNum2 = new BigInteger("987654321987654321987654321");
+		BigInteger result = bigNum1.add(bigNum2);
+		System.out.println("BigInteger addition result : " + result);
+	}
+}
