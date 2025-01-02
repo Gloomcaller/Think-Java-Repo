2d85f4a17037458c4ac104837a316d1581426f35
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-01-02 15:27:45 +0100

    Get/Set

--- Chapter 11 - Designing Classes/src/chapter11sections/TheTimeClass_01.java
+++ Chapter 11 - Designing Classes/src/chapter11sections/TheTimeClass_01.java
@@ -18,6 +18,30 @@ public class TheTimeClass_01 {
 			this.minute = minute;
 			this.second = second;
 		}
+
+		public int getHour() {
+			return this.hour;
+		}
+
+		public int getMinute() {
+			return this.minute;
+		}
+
+		public double getSecond() {
+			return this.second;
+		}
+
+		public void setHour(int hour) {
+			this.hour = hour;
+		}
+
+		public void setMinute(int minute) {
+			this.minute = minute;
+		}
+
+		public void setSecond(double second) {
+			this.second = second;
+		}
 	}
 
 	public static void main(String[] args) {
@@ -26,5 +50,16 @@ public class TheTimeClass_01 {
 
 		System.out.println("Time 1: " + time1.hour + ":" + time1.minute + ":" + time1.second);
 		System.out.println("Time 2: " + time2.hour + ":" + time2.minute + ":" + time2.second);
+
+		time1.setHour(12);
+		time1.setMinute(30);
+		time1.setSecond(0.0);
+
+		time2.setHour(6);
+		time2.setMinute(15);
+		time2.setSecond(30.0);
+
+		System.out.println("Updated Time 1: " + time1.getHour() + ":" + time1.getMinute() + ":" + time1.getSecond());
+		System.out.println("Updated Time 2: " + time2.getHour() + ":" + time2.getMinute() + ":" + time2.getSecond());
 	}
 }
