bb481516eba7a1f94863db19bd35e4dd06b88f09
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-01-02 16:20:24 +0100

    About time...
    The code is finally starting to look like what I'm used to.

--- Chapter 11 - Designing Classes/src/chapter11sections/TheTimeClass_01.java
+++ Chapter 11 - Designing Classes/src/chapter11sections/TheTimeClass_01.java
@@ -17,6 +17,7 @@ public class TheTimeClass_01 {
 			this.hour = hour;
 			this.minute = minute;
 			this.second = second;
+			normalizeTime();
 		}
 
 		public int getHour() {
@@ -43,36 +44,51 @@ public class TheTimeClass_01 {
 			this.second = second;
 		}
 
+		private void normalizeTime() {
+			if (this.second >= 60.0) {
+				this.minute += (int) (this.second / 60);
+				this.second %= 60.0;
+			}
+			if (this.minute >= 60) {
+				this.hour += this.minute / 60;
+				this.minute %= 60;
+			}
+			if (this.hour >= 24) {
+				this.hour %= 24;
+			}
+		}
+
+		public boolean equals(Time that) {
+			final double DELTA = 0.001;
+			return this.hour == that.hour && this.minute == that.minute && Math.abs(this.second - that.second) < DELTA;
+		}
+
+		public Time add(Time t2) {
+			return new Time(this.hour + t2.hour, this.minute + t2.minute, this.second + t2.second);
+		}
+
 		@Override
 		public String toString() {
 			return String.format("%02d:%02d:%04.1f", hour, minute, second);
 		}
 	}
 
-	public static void printTime(Time t) {
-		System.out.println(t.toString());
-	}
-
 	public static void main(String[] args) {
-		Time time1 = new Time();
-		Time time2 = new Time(11, 59, 59.9);
+		Time time1 = new Time(11, 59, 59.9);
+		Time time2 = new Time(6, 15, 30.0);
 
 		System.out.println("Time 1: " + time1);
 		System.out.println("Time 2: " + time2);
 
-		time1.setHour(12);
-		time1.setMinute(30);
-		time1.setSecond(0.0);
+		System.out.println("Are Time 1 and Time 2 equal? " + time1.equals(time2));
 
-		time2.setHour(6);
-		time2.setMinute(15);
-		time2.setSecond(30.0);
+		Time startTime = new Time(18, 50, 0.0);
+		Time runningTime = new Time(2, 16, 0.0);
 
-		System.out.println("Updated Time 1: " + time1);
-		System.out.println("Updated Time 2: " + time2);
+		Time endTime = startTime.add(runningTime);
 
-		System.out.print("Print using printTime method: ");
-		printTime(time1);
-		printTime(time2);
+		System.out.println("Start Time: " + startTime);
+		System.out.println("Running Time: " + runningTime);
+		System.out.println("End Time: " + endTime);
 	}
 }
