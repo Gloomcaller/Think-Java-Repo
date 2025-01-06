c03451d92cb23974cab904f8c9836b19d963dcb7
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-01-06 17:37:42 +0100

    ...

---
+++ Chapter 11 - Designing Classes/src/chapter11exercises/Exercise113.java
@@ -0,0 +1,27 @@
+package chapter11exercises;
+
+public class Exercise113 {
+
+	public static class Date {
+		int year;
+		int month;
+		int day;
+
+		public Date() {
+			this.year = 2000;
+			this.month = 1;
+			this.day = 1;F
+		}
+
+		public Date(int year, int month, int day) {
+			this.year = year;
+			this.month = month;
+			this.day = day;
+		}
+	}
+
+	public static void main(String[] args) {
+		Date mybd = new Date(2001, 2, 8);
+	}
+
+}
