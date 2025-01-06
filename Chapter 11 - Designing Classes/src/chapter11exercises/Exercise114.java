0d0f41c96ab874e7e8da5f7d10495ad2fa2a63e3
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-01-06 20:30:30 +0100

    Quite Rational
    This one was fun to do.

--- Chapter 11 - Designing Classes/src/chapter11exercises/Exercise114.java
+++ Chapter 11 - Designing Classes/src/chapter11exercises/Exercise114.java
@@ -7,7 +7,7 @@ public class Exercise114 {
 		int denominator;
 
 		public Rational() {
-			this.numerator = 0;
+			this.numerator = 1;
 			this.denominator = 1;
 		}
 
@@ -16,12 +16,13 @@ public class Exercise114 {
 			this.denominator = denominator;
 		}
 
-		public static void printRational(Rational rat) {
-			System.out.println(rat.numerator + "/" + rat.denominator);
+		public void printRational() {
+			System.out.println(this.numerator + "/" + this.denominator);
 		}
 
-		public static String toString(Rational rat) {
-			return String.format("%02d:%02d", rat.numerator, rat.denominator);
+		@Override
+		public String toString() {
+			return String.format("%d/%d", numerator, denominator);
 		}
 
 		public void negate() {
@@ -29,20 +30,18 @@ public class Exercise114 {
 		}
 
 		public void invert() {
-			int tempV = this.numerator;
+			int temp = this.numerator;
 			this.numerator = this.denominator;
-			this.denominator = tempV;
+			this.denominator = temp;
 		}
 
-		public static double toDouble(Rational rat) {
-			return (double) rat.numerator / rat.denominator;
+		public double toDouble() {
+			return (double) this.numerator / this.denominator;
 		}
 
 		public Rational reduce() {
 			int gcd = gcd(this.numerator, this.denominator);
-			int reducedNumerator = this.numerator / gcd;
-			int reducedDenominator = this.denominator / gcd;
-			return new Rational(reducedNumerator, reducedDenominator);
+			return new Rational(this.numerator / gcd, this.denominator / gcd);
 		}
 
 		private int gcd(int a, int b) {
@@ -54,25 +53,32 @@ public class Exercise114 {
 			return a;
 		}
 
+		public Rational add(Rational rat) {
+			int newNumerator = this.numerator * rat.denominator + rat.numerator * this.denominator;
+			int newDenominator = this.denominator * rat.denominator;
+			return new Rational(newNumerator, newDenominator).reduce();
+		}
 	}
 
 	public static void main(String[] args) {
 		Rational rat1 = new Rational(5, 6);
-		Rational.printRational(rat1);
+		rat1.printRational();
 
 		rat1.negate();
-		Rational.printRational(rat1);
+		rat1.printRational();
 
 		rat1.invert();
-		Rational.printRational(rat1);
+		rat1.printRational();
 
-		double rat1D = Rational.toDouble(rat1);
-		System.out.println(rat1D);
+		double rat1D = rat1.toDouble();
+		System.out.println("As double: " + rat1D);
 
 		Rational rat2 = new Rational(8, 12);
 		Rational reducedRat = rat2.reduce();
-		System.out.println("Original: " + rat2.numerator + "/" + rat2.denominator);
-		System.out.println("Reduced: " + reducedRat.numerator + "/" + reducedRat.denominator);
-	}
+		System.out.println("Original: " + rat2);
+		System.out.println("Reduced: " + reducedRat);
 
+		Rational sum = rat1.add(rat2);
+		System.out.println("Sum: " + sum);
+	}
 }
