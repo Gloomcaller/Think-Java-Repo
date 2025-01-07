ccbd482a39b392aee0cdfc2ea47f373dc4840d33
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-01-07 20:46:31 +0100

    I love cards.

--- Chapter 12 - Arrays of Objects/src/chapter12sections/Cards_01.java
+++ Chapter 12 - Arrays of Objects/src/chapter12sections/Cards_01.java
@@ -1,24 +1,23 @@
 package chapter12sections;
 
+import java.util.Arrays;
+
 public class Cards_01 {
 
 	public static class Card {
-		private int rank;
-		private int suit;
+		private final int rank;
+		private final int suit;
 
 		public int getRank() {
 			return rank;
-		}
-
-		public void setRank(int rank) {
-			this.rank = rank;
 		}
 
 		public int getSuit() {
 			return suit;
 		}
 
-		public void setSuit(int suit) {
+		public Card(int rank, int suit) {
+			this.rank = rank;
 			this.suit = suit;
 		}
 
@@ -30,18 +29,80 @@ public class Cards_01 {
 			return ret;
 		}
 
-		public Card(int rank, int suit) {
-			this.rank = rank;
-			this.suit = suit;
+		public boolean equals(Card that) {
+			return this.rank == that.rank && this.suit == that.suit;
 		}
+
+		public int compareTo(Card that) {
+			if (this.suit < that.suit) {
+				return -1;
+			}
+			if (this.suit > that.suit) {
+				return 1;
+			}
+			if (this.rank < that.rank) {
+				return -1;
+			}
+			if (this.rank > that.rank) {
+				return 1;
+			}
+			return 0;
+		}
+
+		public static void printDeck(Card[] cards) {
+			for (Card card : cards) {
+				System.out.println(card);
+			}
+		}
+
+		public static int search(Card[] cards, Card target) {
+			for (int i = 0; i < cards.length; i++) {
+				if (cards[i].equals(target)) {
+					return i;
+				}
+			}
+			return -1;
+		}
+
+		public static int binarySearch(Card[] cards, Card target) {
+			int low = 0;
+			int high = cards.length - 1;
+			while (low <= high) {
+				System.out.println(low + ", " + high);
+				int mid = (low + high) / 2;
+				int comp = cards[mid].compareTo(target);
+				if (comp == 0) {
+					return mid;
+				} else if (comp < 0) {
+					low = mid + 1;
+				} else {
+					high = mid - 1;
+				}
+			}
+			return -1;
+		}
+
 	}
 
 	public static void main(String[] args) {
 		Card threeOfClubs = new Card(3, 0);
 
-		Card card = new Card(11, 1);
-		System.out.println(card);
+		Card card1 = new Card(11, 1);
+		System.out.println(card1);
 
+		Card[] cards = new Card[52];
+		int index = 0;
+		for (int suit = 0; suit <= 3; suit++) {
+			for (int rank = 1; rank <= 13; rank++) {
+				cards[index] = new Card(rank, suit);
+				index++;
+			}
+		}
+
+		System.out.println(Arrays.toString(cards));
+
+		Card card2 = new Card(11, 0);
+		System.out.println(Card.binarySearch(cards, card2));
 	}
 
 }
